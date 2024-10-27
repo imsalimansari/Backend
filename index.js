26e83ebf1c0a27311d6b8e6a17d885c0679ddb3d
Salim Ansari <salimansari7644@gmail.com>
2024-10-27 14:24:05 +0530

    First Commit

--- index.js
+++ index.js
@@ -38,3 +38,9 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send({ error: 'Something went wrong!' });
 });
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+});
